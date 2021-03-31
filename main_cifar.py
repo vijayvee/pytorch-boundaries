6e7cd5964cc19e72a1d6850e365b8cf24833eaac
Vijay <vijayvee@gmail.com>
2021-03-31 18:38:30 +0530

    ResNetv1 to ResNetv2, no pretraining for cifar

--- main_cifar.py
+++ main_cifar.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import sys
 
 import numpy as np
 import torch  # pylint: disable=import-error
@@ -10,88 +11,18 @@ import torch.nn as nn  # pylint: disable=import-error
 import torch.nn.functional as F  # pylint: disable=import-error
 import torch.optim as optim  # pylint: disable=import-error
 import torchvision  # pylint: disable=import-error
-import torchvision.models as models
+import torchvision.models as models  # pylint: disable=import-error
 import torchvision.transforms as transforms  # pylint: disable=import-error
+from absl import app, flags
 
-from pytorch_boundaries.models.recurrence.v1net import V1Net
+from pytorch_boundaries.models.resnet_v1net import ResNet18_V1Net
+from pytorch_boundaries.models.preact_resnet import PreActResNet18, PreActResNet50, PreActResNetV1Net18
 
-
-class ResNet18_V1Net(nn.Module):
-  def __init__(self, 
-               timesteps=4,
-               num_classes=10,
-               kernel_size=5,
-               kernel_size_exc=7,
-               kernel_size_inh=3,
-               ):
-    """ResNet with V1Net layer.
-    params:
-      timesteps: Int number of V1Net timesteps
-      num_classes: Int number of output classes
-      kernel_size: Int kernel size of V1Net input convolution.
-      kernel_size_exc: Int kernel size for excitatory V1Net convolution.
-      kernel_size_inh: Int kernel size for inhibitory V1Net convolution.
-    example:
-      >> x = torch.zeros(10, 3, 32, 32).cuda()
-      >> resnet_v1net = ResNet18_V1Net(kernel_size=3,
-                                       kernel_size_exc=7,
-                                       kernel_size_inh=5).cuda()
-      >> out = resnet_v1net(x)
-    """
-    super(ResNet18_V1Net, self).__init__()
-    self.num_classes = num_classes
-    self.timesteps = timesteps
-    self.rgb_mean = np.array((0.485, 0.456, 0.406)) * 255.
-    self.rgb_std = np.array((0.229, 0.224, 0.225)) * 255.
-    # Convert to n, c, h, w
-    self.rgb_mean = self.rgb_mean.reshape((1, 3, 1, 1))
-    self.rgb_mean = torch.Tensor(self.rgb_mean).float().cuda()
-    self.rgb_std = self.rgb_std.reshape((1, 3, 1, 1))
-    self.rgb_std = torch.Tensor(self.rgb_std).float().cuda()
-    model = models.resnet18(pretrained=True).cuda()
-    self.resnet_conv_1 = self.extract_layer(model, 
-                                            'resnet18',
-                                            'retina')
-    self.v1net_conv = V1Net(64, 64, 
-                            kernel_size,
-                            kernel_size_exc, 
-                            kernel_size_inh)
-    self.resnet_conv_2 = self.extract_layer(model,
-                                            'resnet18',
-                                            'cortex')
-    self.fc = nn.Linear(512, num_classes)
-
-  def standardize(self, inputs):
-    """Mean normalize input images."""
-    # do not normalize CIFAR-10 images, normalization added to dataloader
-    return inputs
-  
-  def forward(self, features):
-    net = self.standardize(features)
-    net = self.resnet_conv_1(net)
-    n, c, h, w = net.shape
-    net_tiled = net.repeat(self.timesteps, 1, 1, 1, 1).view(self.timesteps, n, c, h, w)
-    net_tiled = torch.transpose(net_tiled, 1, 0)
-    _, (net, _) = self.v1net_conv(net_tiled)
-    net = self.resnet_conv_2(net)
-    net = torch.flatten(net, 1)
-    net = self.fc(net)
-    return net
-
-  def extract_layer(self, model, 
-                    backbone_mode, 
-                    key):
-    if backbone_mode == 'resnet18':
-      index_dict = {
-          'retina': (0,4), 
-          'cortex': (4,9),
-      }
-    start, end = index_dict[key]
-    modified_model = nn.Sequential(*list(
-      model.children()
-      )[start:end])
-    return modified_model
-
+remove_v1net = True
+test_run = False
+timesteps = 0
+write_results = None  # "cifar_predictions/resnet18_v1net_predictions_eval_t_%s_remove_v1net_%s" % (timesteps, remove_v1net)
+checkpoint = None  # checkpoint/ckpt_4steps_reg_remove_v1net_True.pth"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -103,20 +34,22 @@ transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), 
+                         (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), 
+                         (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = torchvision.datasets.CIFAR100(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.CIFAR100(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
@@ -127,15 +60,22 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = models.resnet18(pretrained=True)
-net = ResNet18_V1Net(kernel_size=3, kernel_size_exc=7, kernel_size_inh=5)
+# net = ResNet18_V1Net(kernel_size=3, kernel_size_exc=7, 
+#                      kernel_size_inh=5, timesteps=timesteps,
+#                      remove_v1net=remove_v1net)
+net = PreActResNetV1Net18(num_classes=100, kernel_size=3, 
+                          kernel_size_exc=5, kernel_size_inh=3, 
+                          timesteps=3)
+# net = PreActResNet18(num_classes=100)
 net = net.to(device)
 if device == 'cuda':
   # net = torch.nn.DataParallel(net)
   cudnn.benchmark = True
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=5e-4,
-                      momentum=0.9, weight_decay=1e-4)
+optimizer = optim.SGD(net.parameters(), lr=1e-2,
+                       momentum=0.9, nesterov=True,
+                       weight_decay=1e-3)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
 # Training
@@ -167,13 +107,24 @@ def train(epoch):
 def test(epoch):
   global best_acc
   net.eval()
+  if checkpoint:
+    state_dict = torch.load(checkpoint)
+    print("Loading from %s with accuracy %s" % (checkpoint, 
+                                                state_dict["acc"]))
+    net.load_state_dict(state_dict['net'], strict=False)
   test_loss = 0
   correct = 0
   total = 0
+  np_predictions = []
+  np_targets = []
+
   with torch.no_grad():
     for batch_idx, (inputs, targets) in enumerate(testloader):
       inputs, targets = inputs.to(device), targets.to(device)
       outputs = net(inputs)
+      if write_results:
+        np_predictions.extend(outputs.cpu().numpy())
+        np_targets.extend(targets.cpu().numpy())
       loss = criterion(outputs, targets)
 
       test_loss += loss.item()
@@ -182,6 +133,13 @@ def test(epoch):
       correct += predicted.eq(targets).sum().item()
     print('Test Loss: %.3f | Acc: %.3f%% (%d/%d)'
                 % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    acc = 100.*correct/total
+    if write_results:
+      np.array(np_predictions).dump(
+            open('%s_acc_%.3f_predictions.npy' % (write_results, acc), 'wb'))
+      np.array(np_targets).dump(
+            open('%s_acc_%.3f_labels.npy' % (write_results, acc), 'wb'))
+      return
 
   # Save checkpoint.
   acc = 100.*correct/total
@@ -194,12 +152,21 @@ def test(epoch):
     }
     if not os.path.isdir('checkpoint'):
       os.mkdir('checkpoint')
-    torch.save(state, './checkpoint/ckpt.pth')
+    torch.save(state,
+      './checkpoint/cifar100_ckpt_0steps_reg_remove_v1net_%s_run2.pth' % remove_v1net)
     best_acc = acc
 
 
+def main():
+  if test_run:
+    test(-1)
+  else:
+    for epoch in range(start_epoch, start_epoch+200):
+      train(epoch)
+      test(epoch)
+      if epoch > 0 and epoch % 10 == 0:
+        for param_group in optimizer.param_groups:
+          param_group['lr'] /= 2.
+
 if __name__=="__main__":
-  for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
+  main()
